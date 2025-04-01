5b17ea7d1e27552077b0c61fa61f7b31da51d97e
Luk Geerts <119401863+LukGeertsLgix@users.noreply.github.com>
2025-04-01 16:24:43 +0200

    Update light.py
    Replaced import of COLOR_MODE_BRIGHTNESS and COLOR_MODE_ONOFF with import ColorMode
    
    As it will be deprecated in HomeAssistant
    
    In code updated de color_mode:
    ColorMode.BRIGHTNESS
    ColorMode.ONOFF

--- custom_components/teletask/light.py
+++ custom_components/teletask/light.py
@@ -6,9 +6,8 @@ from .const import (
 )
 
 from homeassistant.components.light import (
+    ColorMode,
     ATTR_BRIGHTNESS,
-    COLOR_MODE_BRIGHTNESS,
-    COLOR_MODE_ONOFF,
     LightEntity,
 )
 #from homeassistant.components.teletask import DATA_TELETASK
@@ -91,8 +90,8 @@ class TeletaskLight(LightEntity):
     def color_mode(self):
         """Return the color mode of the light."""
         if self.device.supports_brightness:
-            return COLOR_MODE_BRIGHTNESS
-        return COLOR_MODE_ONOFF
+            return ColorMode.BRIGHTNESS
+        return ColorMode.ONOFF
 
     @property
     def supported_color_modes(self):
